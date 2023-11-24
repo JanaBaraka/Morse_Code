93cdd33f0b3013c4df2cd0f3192a7b195569879c
JanaBaraka <145542559+JanaBaraka@users.noreply.github.com>
2023-11-24 12:00:32 +0200

    added an encrypt funciton
    A function that encrypts english words to morse code

--- morse-code.py
+++ morse-code.py
@@ -8,3 +8,11 @@ morse_code_dict = { #alpahbet mapping dictionary
     '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
     '$': '...-..-', '@': '.--.-.', ' ': '/'
 }
+
+def encrypt(text):
+    encrypted = [] # an empty list used to store each character mapped from the dict
+    for char in text.upper():
+        if char in morse_code_dict:
+            encrypted.append(morse_code_dict[char]) 
+    return ' '.join(encrypted) #joins the chars to the list with a space between them
+
